5ab2320433a016afba77cc665393471cc9ce2b98
privatefree00 <146465893+privatefree00@users.noreply.github.com>
2026-04-03 20:00:52 +0700

    Create [[middleware]].js

---
+++ functions/[[middleware]].js
@@ -0,0 +1,30 @@
+export const onRequest = async (context) => {
+  const url = new URL(context.request.url);
+
+  // Daftar rute yang ingin diproses
+  const allowedRoutes = [
+    "/beatriz", 
+    "/rebeca"
+  ];
+
+  if (allowedRoutes.includes(url.pathname)) {
+    const visitor = {
+      ip: context.request.headers.get('cf-connecting-ip'),
+      country: context.request.headers.get('cf-ipcountry'),
+      url: url.href,
+      timestamp: new Date().toISOString(),
+    };
+
+    // Kirim data ke Worker milikmu
+    await fetch("https://newrepot.masenyong66.workers.dev/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(visitor)
+    });
+  }
+
+  // Lanjut ke halaman tujuan
+  return context.next();
+};
